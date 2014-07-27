66ea7689e486c1ab6c5b22eef400364de26cba85
christianrochford <rochfordchristian@gmail.com>
2014-07-27 14:12:03 +0100

    add mobile menu

--- js/app/app.js
+++ js/app/app.js
@@ -1,4 +1,31 @@
 
 $(document).ready(function(){
-	console.log('hey!');
+	$('#mobile-open').click(function(e){
+		e.preventDefault();
+		$(this).hide();
+		$('#mobile-close').show();
+		$('nav').addClass('open');
+	});
+	$('#mobile-close').click(function(e){
+		e.preventDefault();
+		$(this).hide();
+		$('#mobile-open').show();
+		$('nav').removeClass('open');
+
+	});
+	if($(window).width()>740){
+		$('#mobile-open, #mobile-close').hide();
+		$('nav').fadeIn();
+	} else {
+		$('#mobile-open').show();
+	}
+});
+
+$(window).resize(function(){
+	if($(window).width()>740){
+		$('#mobile-open, #mobile-close').hide();
+		$('nav').fadeIn();
+	} else {
+		$('#mobile-open').show();
+	}
 });
