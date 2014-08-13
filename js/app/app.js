0082c9c8b821a81241044bdd80a50aba1c92695f
christianrochford <rochfordchristian@gmail.com>
2014-08-13 23:00:53 +0100

    add isotope filters

--- js/app/app.js
+++ js/app/app.js
@@ -1,34 +1,5 @@
 $(document).ready(function() {
 
-  // Initialise Carousel
-  $('.carousel').slick({
-    arrows: true,
-    dots: false,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    responsive: [{
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-      }
-    }, {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    }, {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }]
-  });
-
   // Set page height
   $('#home, #about, #team, #contact').height($(window).height());
 
@@ -80,13 +51,42 @@ $(document).ready(function() {
     }
   );
 
+  // Initialise Carousel
+  $('.carousel').slick({
+    arrows: true,
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      }
+    }, {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }, {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }]
+  });
+
   // Fitvids
   $(".work-video").fitVids();
 
   // Work Videos
   $('.work-video.commercials, .work-video.entertainment, .work-video.production, .work-video.interactive').hide();
   $('.projects-link.commercials, .projects-link.entertainment, .projects-link.production, .projects-link.interactive').hide();
-  $('.gallery-link').click(function(e){
+  $('.gallery-link').click(function(e) {
     e.preventDefault();
     console.log('HEY!');
     var href = $(this).attr("href");
@@ -97,12 +97,161 @@ $(document).ready(function() {
     $('.projects-link.' + hash).show();
   });
 
+  // Projects Gallery
+  var $container = $('#gallery'),
+    filters = {},
+    colW = 226;
+  $container.isotope({
+    itemSelector: '.project',
+    filter: ''
+  });
 
+  if(window.location.hash){
+    var hash = window.location.hash.substring(1);
+    $('.filter a').each(function(){
+      var filterVal = $(this).attr('data-filter-value');
+      if(filterVal == '.' + hash){
+        $(this).click();
+      }
+    });
+  }
+  
+
+  // filter links
+  $('.filter a').click(function() {
+    var $this = $(this);
+
+    var $optionSet = $this.parents('.option-set');
+    // change selected class
+    $optionSet.find('.selected').removeClass('selected');
+    $this.addClass('selected');
+
+    // store filter value in object
+    // i.e. filters.color = 'red'
+    var group = $optionSet.attr('data-filter-group');
+    filters[group] = $this.attr('data-filter-value');
+
+    // convert object into array
+    var isoFilters = [];
+    for (var prop in filters) {
+      isoFilters.push(filters[prop])
+    }
+    var selector = isoFilters.join('');
+    $container.isotope({
+      filter: selector
+    });
+    return false;
+  });
+
+});
+
+$(window).load(function() {
+  // Center about content
+  var windowHeight = ($(window).height() - 200);
+  var aboutHeight = $('#about').height();
+  var aboutMargin = ((windowHeight - aboutHeight) / 2);
+  if (aboutMargin > 0) {
+    $('#about').css({
+      paddingTop: aboutMargin
+    });
+  } else {
+    $('#about').css({
+      paddingTop: 110
+    });
+  }
+  // Center team content
+  var windowHeight = ($(window).height() - 200);
+  var teamHeight = $('#team').height();
+  var teamMargin = ((windowHeight - teamHeight) / 2);
+  if (teamMargin > 0) {
+    $('#team').css({
+      paddingTop: teamMargin
+    });
+  } else {
+    $('#team').css({
+      paddingTop: 110
+    });
+  }
+  // Center work content
+  var workHeight = $('#work').height();
+  var workMargin = ((windowHeight - workHeight) / 2);
+  if (workMargin > 0) {
+    $('#work').css({
+      paddingTop: workMargin
+    });
+  } else {
+    $('#work').css({
+      paddingTop: 110
+    });
+  }
+  // Center contact content
+  var contactHeight = $('#contact').height();
+  var contactMargin = ((windowHeight - contactHeight) / 2);
+  if (contactMargin > 0) {
+    $('#contact').css({
+      marginTop: contactMargin
+    });
+  } else {
+    $('#contact').css({
+      paddingTop: 110
+    });
+  }
 });
 
 $(window).resize(function() {
 
   $('#home, #about, #team, #contact').height($(window).height());
+
+  // Center about content
+  var windowHeight = ($(window).height() - 200);
+  var aboutHeight = $('#about').height();
+  var aboutMargin = ((windowHeight - aboutHeight) / 2);
+  if (aboutMargin > 0) {
+    $('#about').css({
+      paddingTop: aboutMargin
+    });
+  } else {
+    $('#about').css({
+      paddingTop: 110
+    });
+  }
+  // Center team content
+  var windowHeight = ($(window).height() - 200);
+  var teamHeight = $('#team').height();
+  var teamMargin = ((windowHeight - teamHeight) / 2);
+  if (teamMargin > 0) {
+    $('#team').css({
+      paddingTop: teamMargin
+    });
+  } else {
+    $('#team').css({
+      paddingTop: 110
+    });
+  }
+  // Center work content
+  var workHeight = $('#work').height();
+  var workMargin = ((windowHeight - workHeight) / 2);
+  if (workMargin > 0) {
+    $('#work').css({
+      paddingTop: workMargin
+    });
+  } else {
+    $('#work').css({
+      paddingTop: 110
+    });
+  }
+  // Center contact content
+  var contactHeight = $('#contact').height();
+  var contactMargin = ((windowHeight - contactHeight) / 2);
+  if (contactMargin > 0) {
+    $('#contact').css({
+      paddingTop: contactMargin
+    });
+  } else {
+    $('#contact').css({
+      paddingTop: 110
+    });
+  }
 
   if ($(window).width() > 740) {
     $('#mobile-open, #mobile-close').hide();
